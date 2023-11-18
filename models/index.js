87a7b170e471bba40f33b19373649719e228e32a
Veronica Perez <veperez@live.com>
2023-11-18 10:46:46 -0600

    built framework from group project structure, changed areas where needed

---
+++ models/index.js
@@ -0,0 +1,36 @@
+//use inde.js from group project and change names
+const User = require('./user');
+const Post = require('./post');
+const Comment = require('./comment');
+
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+})
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+})
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+})
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+})
+
+
+module.exports = {
+    User,
+    Post,
+    Comment
+};
